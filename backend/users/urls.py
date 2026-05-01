9c0c40af0db0cf0e8ae56482ed4ad211e53dc48a
Navan <navan05please@gmail.com>
2026-05-01 16:57:45 +0530

    completed kubernetees part

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,6 +3,7 @@ from .views import RegisterView, ProfileView
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
+    path('', ProfileView.as_view(), name='profile'),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('profile/', ProfileView.as_view(), name='profile'),
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
+import dotenv from "dotenv";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  // baseURL: import.meta.env.VITE_BACKEND_URI || "http://127.0.0.1:8000/api/",
+  baseURL:`${import.meta.env.VITE_BACKEND_URI}/api/`
 });
 
 const PUBLIC_ROUTES = ["register/", "login/"];
